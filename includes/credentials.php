4e72cf7292b5dad3a2d032e27a68746a8ee17e36
Bill Newman <william.newman@seattlecolleges.edu>
2015-12-02 17:18:52 -0800

    Adds admin login

---
+++ includes/credentials.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * credentials_inc.php stores db connection info safe from prying eyes
+ * 
+ *  
+ * @package nmCommon
+ * @author Bill Newman <williamnewman@gmail.com>
+ * @version 2.0 2010/07/22
+ * @link http://www.newmanix.com/ 
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
+ * @todo none
+ */
+
+//place credentials here:
+define('DB_NAME','seacentral_db');
+define('DB_USER','seacentral_admin');
+define('DB_PASSWORD','1600broadway');
+define('DB_HOST','seacentral.newmanix.com'); 
